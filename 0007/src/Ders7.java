53a78867679d5f79fdfedc84a64841d910176235
ÖFFK <110122698+FatihKarakass@users.noreply.github.com>
2022-11-19 00:25:14 +0300

    Update Ders7.java

--- 0007/src/Ders7.java
+++ 0007/src/Ders7.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Ders7 {
 	
 	public static void main(String[] args){
-		System.out.println("1-Sonuç :" + ((3>4) && ("yazýlým"=="yazýlým")));
+		//System.out.println("1-Sonuç :" + ((3>4) && ("yazýlým"=="yazýlým")));
 		
 		String kullanýcý_adý ="öffk";
 		String parola ="1234";
@@ -18,6 +18,20 @@ public class Ders7 {
 		System.out.println("Paraola:");
 		String parola2=scan.nextLine();
 		
+		if( !(kullanýcý_adý.equals(kullanýcý_adý2))&& !(parola.equals(parola2)){
+			System.out.println("Kullanýcý adý ve parola yanlýþ!");
+			
+		}
+		else if((kullanýcý_adý.equals(kullanýcý_adý2))&& !(parola.equals(parola2)){
+			System.out.println("Parola yanlýþ!");
+		}
+		else if(!(kullanýcý_adý.equals(kullanýcý_adý2))&& (parola.equals(parola2)){
+			System.out.println("Kullanýcý adý yanlýþ!");
+		}
+		else ((kullanýcý_adý.equals(kullanýcý_adý2))&& (parola.equals(parola2)){
+			System.out.println("Giriþ baþarýlý...");
+
+		}
 	}
 
 }
