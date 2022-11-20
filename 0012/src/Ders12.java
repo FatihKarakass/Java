6a635ae933ad420e734581afecf392caed40725d
ÖFFK <110122698+FatihKarakass@users.noreply.github.com>
2022-11-20 14:26:29 +0300

    Add files via upload

---
+++ 0012/src/Ders12.java
@@ -0,0 +1,13 @@
+
+public class Ders12 {
+	 public static void main(String[] args){
+		 
+		 
+		/* for(int i=0 , j=0; i<10 || j<5; i++,j++){
+			 System.out.println("i=" +i + "  j=" + j);
+		 }
+		 */
+		 
+		 
+	 }
+}
---
+++ 0013/src/Ders13.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class Ders13 {
+	public static void main(String[] args){
+		/*
+	while(true){
+		Scanner scan =new Scanner(System.in);
+		String s;
+		System.out.println("karakter giriniz: ");
+		s= scan.nextLine();
+		
+		if(s.equals("q")){
+			System.out.println("programdan çýkýlýyor.....");
+			
+			break;
+		}
+		System.out.println("karakter= " +s);
+		
+		}
+		*/
+		int i =0;
+		while(i<10){
+			if(i==4||i==7){
+				i++;
+				continue;
+			}
+			System.out.println("i:" +i);
+			i++;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
