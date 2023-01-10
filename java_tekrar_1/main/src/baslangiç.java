9cb410f010535fb14e841f973d63d392c00ed3db
ÖFFK <110122698+FatihKarakass@users.noreply.github.com>
2023-01-10 22:07:41 +0300

    java tekrar 1

---
+++ java_tekrar_1/main/src/baslangiç.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class baslangiç {
+    private static Scanner input;
+
+    public static void main(String[] args) {
+        /*int numberOne,numberTwo=2,nomberThree;
+        System.out.print(numberTwo);*/
+
+        /*int kisaKenar =10,uzunKenar=20;
+        int alan= kisaKenar*uzunKenar;
+
+        int cevre= 2 *(kisaKenar+uzunKenar);
+
+        System.out.println(alan ) ;*/
+
+        /*
+
+            Toplama : a + b
+            Çıkarma : a – b
+            Çarpma : a * b
+            Bölme : a / b
+            Mod alma : a % b
+            1 arttırma : a++
+            1 eksiltme : b--
+            Karşılaştırma Operatörleri
+            Java'da Karşılaştırma Operatörleri iki nesnenin birbirleriyle olan durumlarını belirler.
+
+            Eşitlik : a == b
+            Eşit Değil : a != b
+            Büyüktür : a > b
+            Küçüktür : a < b
+            Büyük Eşittir : a >= b
+            Küçük Eşittir : a <= b
+            Mantıksal Operatörler
+            Java'da Mantıksal Operatörler , nesnelerin veya ifadelerin mantıksal değerlerini yansıtır.
+
+            Ve : a && b
+            Veya : a || b
+            Değil : !(a&&b)
+            Koşul Operatörü
+            Java'da Koşul Operatörleri ifadelerin sonucunda oluşacak olayları belirler.
+
+            a = 5 ;
+            b = (a == 1) ? 1 : 0
+            Çıktısı : 0
+
+            */
+
+
+        //kullaıcıda veri alma
+        /*
+        Scanner input= new Scanner(System.in);
+        int a,b;
+
+        System.out.println("A sayısını giriniz");
+        a = input.nextInt();
+        System.out.println("A sayısı:"+a);
+*/
+
+
+
+
+    }
+}
